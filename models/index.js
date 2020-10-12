b25d49cc925884609b9895eb9b414fff033ca175
danielDev <valcyum.085@gmail.com>
2020-10-12 00:46:51 -0400

    refactor

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const dbConfig = require("../config/bd.config.js");
 
 const Sequelize = require("sequelize");
-const { dialect } = require("../config/bd.config.js");
+//const { dialect } = require("../config/bd.config.js");
 const sequelize = new Sequelize(dbConfig.db, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST, dialect: dbConfig.dialect, operatorAliases: false,
 
