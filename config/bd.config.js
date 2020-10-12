49c9123de32a5c402c475d9d56f8e449c2f36dcf
danielDev <valcyum.085@gmail.com>
2020-10-12 15:41:08 -0400

    probando ruta en cors origin para heroku7

--- config/bd.config.js
+++ config/bd.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     HOST : "ec2-54-90-68-208.compute-1.amazonaws.com",
-    USER : "dcejojok8qtdk7",
+    USER : "mfmdswasawfhna",
     PASSWORD : "6eaf9dd30535b2a3556eb59cadbf4b289dfbd7cb6e7914ca565c7d31710736d5",
     DB : "mfmdswasawfhna",
     dialect: "postgres",
